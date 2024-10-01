dd74357118f93e7ab2f59ed8a0477666a7cef551
mariambingoo <mariamsheta13@gmail.com>
2024-10-01 12:06:23 +0300

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-     const task = true;
-     const task2 = false;
+     task; 
+     task2;
   }
 
   return [task, task2];
